11485c5d8e17e1fad1a1c0cca5da07a901541e12
Wen yifei <602254243@qq.com>
2026-07-21 00:53:04 +0800

    First comit initialization

---
+++ src/main/java/com/wyfagent/mixagent/MixAgentApplication.java
@@ -0,0 +1,13 @@
+package com.wyfagent.mixagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyfagent/mixagent/MixAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyfagent.mixagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
